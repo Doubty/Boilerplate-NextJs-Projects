02efb212d48497e1591457e9874864cd85c753bc
doubty <goantonio80@gmail.com>
2023-01-01 16:43:32 -0300

    Husky teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const teste = 'testando'
+const testehusky = 'testando'
 
 export default function Home() {
   return (
