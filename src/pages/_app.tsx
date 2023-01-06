b60a95499f8db667a9078444cc7a72791def812b
doubty <goantonio80@gmail.com>
2023-01-06 01:11:27 -0300

    Adjust in the project and configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import GlobalStyle from 'styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Boilerplate React by Galvs - Doubty</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project created to work with typescript, react and Styled components"
+        />
       </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   )
