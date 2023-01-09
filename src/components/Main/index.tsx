f05bb65235f70dae806cf5307568a5fd514fc7a9
doubty <goantonio80@gmail.com>
2023-01-08 23:02:57 -0300

    Added new component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,19 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Logo do projeto - Boilerplate" />
+    <S.Title>BoilerPlate do Galvs</S.Title>
+    <S.Description>
+      Boilerplate criado com amor e dedicação total de Antônio Galvão.
+      Boilerplate conta com projeto em Next.js usando typescript e styled
+      componentes, bem como, configurado para testes, prettier, eslint e husky.
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor na frente do computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
