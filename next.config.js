98d94d05b0efc435f29fc943d53c1e238456b159
doubty <goantonio80@gmail.com>
2023-01-18 23:46:03 -0300

    configured pwa to next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* eslint-disable @typescript-eslint/no-var-requires */
+const isProd = process.env.NODE_ENV === 'production'
+const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd,
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/]
+})
 
+const nextConfig = withPWA({
+  // next config
+})
 module.exports = nextConfig
