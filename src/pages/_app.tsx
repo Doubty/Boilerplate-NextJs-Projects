ac1eaeda0ea51e40aca5fe6fc685933b3ba57165
doubty <goantonio80@gmail.com>
2023-01-18 23:44:37 -0300

    Added the manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Boilerplate React by Galvs - Doubty</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project created to work with typescript, react and Styled components"
